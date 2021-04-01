a090b7188378cac6410ed118a3fa976a4287ba40
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-04-01 18:52:20 +0530

    cors mapping added removed extra param

--- platform-core/src/main/java/com/cz/platform/cors/CORSConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/cors/CORSConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class CORSConfiguration {
