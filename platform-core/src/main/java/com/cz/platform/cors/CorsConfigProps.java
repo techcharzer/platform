52eb408d399483f53558e5c339bca676368fc229
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-04-01 17:29:54 +0530

    Added the configuration for cors

--- platform-core/src/main/java/com/cz/platform/cors/CorsConfigProps.java
+++ platform-core/src/main/java/com/cz/platform/cors/CorsConfigProps.java
@@ -1,14 +1,18 @@
 package com.cz.platform.cors;
 
+import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
 
 @Data
 @Configuration
+@RefreshScope
 @ConfigurationProperties(prefix = "app.cors.config")
 public class CorsConfigProps {
-	private boolean disabled;
 	private String allowedOrigin;
+	private List<String> allowedOrigins;
 }
