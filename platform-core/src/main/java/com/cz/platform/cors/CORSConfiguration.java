579cadae1d0c373c8a905b0239deff97b21604d6
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-04-02 11:14:31 +0530

    removed cors settings

--- platform-core/src/main/java/com/cz/platform/cors/CORSConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/cors/CORSConfiguration.java
@@ -1,31 +1,31 @@
-package com.cz.platform.cors;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Configuration
-@Slf4j
-public class CORSConfiguration {
-
-	@Autowired
-	private CorsConfigProps props;
-
-	@Bean
-	public CorsConfigurationSource addCorsMappings() {
-		log.info("cors configuring : {}", props.getAllowedOrigins());
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOriginPatterns(props.getAllowedOrigins());
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
-}
+//package com.cz.platform.cors;
+//
+//import java.util.Arrays;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Configuration
+//@Slf4j
+//public class CORSConfiguration {
+//
+//	@Autowired
+//	private CorsConfigProps props;
+//
+//	@Bean
+//	public CorsConfigurationSource addCorsMappings() {
+//		log.info("cors configuring : {}", props.getAllowedOrigins());
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOriginPatterns(props.getAllowedOrigins());
+//		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
+//}
