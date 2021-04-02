ad9d673c4c7e9c89f322fefec134a7571a15d805
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-04-02 12:03:07 +0530

    Added new configuration for

--- platform-core/src/main/java/com/cz/platform/cors/CORSConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/cors/CORSConfiguration.java
@@ -1,31 +1,63 @@
-//package com.cz.platform.cors;
-//
-//import java.util.Arrays;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Configuration
-//@Slf4j
-//public class CORSConfiguration {
-//
-//	@Autowired
-//	private CorsConfigProps props;
-//
-//	@Bean
-//	public CorsConfigurationSource addCorsMappings() {
-//		log.info("cors configuring : {}", props.getAllowedOrigins());
-//		CorsConfiguration configuration = new CorsConfiguration();
-//		configuration.setAllowedOriginPatterns(props.getAllowedOrigins());
-//		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", configuration);
-//		return source;
-//	}
-//}
+package com.cz.platform.cors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.cz.platform.exception.PlatformExceptionCodes;
+import com.cz.platform.exception.ValidationException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class CORSConfiguration {
+
+	@Autowired
+	private CorsConfigProps props;
+
+	@Bean
+	public WebMvcConfigurer configurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				log.info("CORS configuration allowedOrigin : {}", props.getAllowedOrigins());
+				CorsRegistration registration = registry.addMapping("/**");
+				registration.allowedHeaders("*");
+				int i = 0;
+				if (props.getAllowedOrigins().size() > 6) {
+					throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(),
+							"Invalid cors configuration. max allowed 6 allowed origins.");
+				}
+				if (props.getAllowedOrigins().size() == 1) {
+					registration.allowedOriginPatterns(props.getAllowedOrigins().get(i++));
+				} else if (props.getAllowedOrigins().size() == 2) {
+					registration.allowedOriginPatterns(props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++));
+				} else if (props.getAllowedOrigins().size() == 3) {
+					registration.allowedOriginPatterns(props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++), props.getAllowedOrigins().get(i++));
+				} else if (props.getAllowedOrigins().size() == 4) {
+					registration.allowedOriginPatterns(props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++), props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++));
+				} else if (props.getAllowedOrigins().size() == 5) {
+					registration.allowedOriginPatterns(props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++), props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++), props.getAllowedOrigins().get(i++));
+				} else if (props.getAllowedOrigins().size() == 6) {
+					registration.allowedOriginPatterns(props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++), props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++), props.getAllowedOrigins().get(i++),
+							props.getAllowedOrigins().get(i++));
+				}
+				registration.allowedMethods("POST", "PUT", "GET", "OPTIONS");
+				registration.allowCredentials(true);
+			}
+		};
+	}
+
+}
