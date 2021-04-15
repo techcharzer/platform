fc44759fd8eecbce0a5fd09da3f83ab5be1290c3
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-04-15 12:30:12 +0530

    test sentry exception logging.

---
+++ platform-core/src/main/java/com/cz/platform/logging/TestSentryException.java
@@ -0,0 +1,26 @@
+package com.cz.platform.logging;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import com.cz.platform.PlatformConstants;
+import com.cz.platform.custom.events.CustomSpringEvent;
+import com.cz.platform.exception.ApplicationException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Profile(PlatformConstants.PROD_PROFILE)
+public class TestSentryException {
+
+	@EventListener(condition = "#event.name == 'TEST_SENTRY_EXCEPTION_LOGGING'")
+	private void testSentryExceptoin(CustomSpringEvent event) throws ApplicationException {
+		try {
+			throw new Exception("This is a test exception.");
+		} catch (Exception e) {
+			log.error("test exceptoin", e);
+		}
+	}
+}
