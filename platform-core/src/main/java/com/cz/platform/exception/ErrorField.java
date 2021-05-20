9ab7c988175f439df6921e1cc2bc421337b926c6
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-05-20 13:43:06 +0530

    renamed errorMessage message.

--- platform-core/src/main/java/com/cz/platform/exception/ErrorField.java
+++ platform-core/src/main/java/com/cz/platform/exception/ErrorField.java
@@ -2,42 +2,20 @@ package com.cz.platform.exception;
 
 import java.io.Serializable;
 
-import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-@ToString
+@Data
+@AllArgsConstructor
 public class ErrorField implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 13234548716666666L;
 	private String code;
-	private String errorMessage;
-
-	public ErrorField(String code, String errors) {
-		this.code = code;
-		this.errorMessage = errors;
-	}
-
-	public void setErrorMessage(String error) {
-		this.errorMessage = error;
-	}
-
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
+	private String message;
 
 	public ErrorField(IExceptionCodes codes) {
 		this.code = codes.getCode();
-		this.errorMessage = codes.getMessage();
+		this.message = codes.getMessage();
 	}
 
 }
