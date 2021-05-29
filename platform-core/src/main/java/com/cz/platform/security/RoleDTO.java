a7bc064a678fcdbd5b3f85b8dd160a0767c63b50
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-05-29 16:50:03 +0530

    Added role DTO.

--- platform-core/src/main/java/com/cz/platform/security/RoleDTO.java
+++ platform-core/src/main/java/com/cz/platform/security/RoleDTO.java
@@ -1,11 +1,16 @@
 package com.cz.platform.security;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class RoleDTO {
+public class RoleDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 176382974834L;
 	private String roleId;
 	private List<String> permissions;
 }
