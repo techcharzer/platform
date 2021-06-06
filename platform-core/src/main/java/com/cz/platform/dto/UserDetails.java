87050681e46e26ba00c9616c11697ac7f5f63528
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-06-06 14:52:22 +0530

    removed roles.

--- platform-core/src/main/java/com/cz/platform/dto/UserDetails.java
+++ platform-core/src/main/java/com/cz/platform/dto/UserDetails.java
@@ -1,8 +1,6 @@
 package com.cz.platform.dto;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.cz.platform.security.ChargerNetworkUserMapping;
 
@@ -13,7 +11,6 @@ public class UserDetails {
 	private String userId;
 	private String mobileNumber;
 	private String email;
-	private List<String> roles = new ArrayList<>();
 	private ChargerNetworkUserMapping society;
 	private LocalDateTime ceatedAt;
 	private LocalDateTime updatedAt;
