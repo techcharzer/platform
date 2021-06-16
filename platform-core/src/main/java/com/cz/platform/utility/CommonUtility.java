d94e2bd761a261737584f7114ca6480b714d7409
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-06-16 12:16:48 +0530

    added a utility method for creating url slug.

--- platform-core/src/main/java/com/cz/platform/utility/CommonUtility.java
+++ platform-core/src/main/java/com/cz/platform/utility/CommonUtility.java
@@ -7,6 +7,10 @@ public final class CommonUtility {
 	private CommonUtility() {
 	}
 
+	public static String getUrlSlug(String str) {
+		return ObjectUtils.isEmpty(str) ? str : replaceSpaceWithHyphen(str.toLowerCase());
+	}
+
 	public static String replaceSpaceWithHyphen(String str) {
 		return ObjectUtils.isEmpty(str) ? str : str.replace(" ", "-");
 	}
