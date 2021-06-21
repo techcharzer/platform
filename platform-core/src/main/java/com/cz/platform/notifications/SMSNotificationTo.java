9440e77a3fbf51c04b15f8e703e2b21fddd1bae1
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-06-21 20:07:47 +0530

    Added default serialization.

--- platform-core/src/main/java/com/cz/platform/notifications/SMSNotificationTo.java
+++ platform-core/src/main/java/com/cz/platform/notifications/SMSNotificationTo.java
@@ -1,8 +1,14 @@
 package com.cz.platform.notifications;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class SMSNotificationTo implements NotificationTO {
+public class SMSNotificationTo implements NotificationTO, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5908222877037206420L;
 	private String mobileNumber;
 }
--- platform-core/src/main/java/com/cz/platform/notifications/WhatsappNotificationTo.java
+++ platform-core/src/main/java/com/cz/platform/notifications/WhatsappNotificationTo.java
@@ -1,9 +1,15 @@
 package com.cz.platform.notifications;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class WhatsappNotificationTo implements NotificationTO {
+public class WhatsappNotificationTo implements NotificationTO, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7008315795908771996L;
 	private String phone;
 	private Boolean isWaitingForResponse = Boolean.FALSE;
 }
