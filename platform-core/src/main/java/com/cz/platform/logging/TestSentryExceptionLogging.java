4336b30884d21a89fc17ee30d750a30d42a55892
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-08-20 12:18:12 +0530

    added sentry exception.

--- platform-core/src/main/java/com/cz/platform/logging/TestSentryExceptionLogging.java
+++ platform-core/src/main/java/com/cz/platform/logging/TestSentryExceptionLogging.java
@@ -1,10 +1,10 @@
 package com.cz.platform.logging;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.EventListener;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.cz.platform.PlatformConstants;
 import com.cz.platform.custom.events.CustomSpringEvent;
 import com.cz.platform.exception.ApplicationException;
 
@@ -12,15 +12,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@Profile(PlatformConstants.PROD_PROFILE)
 public class TestSentryExceptionLogging {
 
-	@EventListener(condition = "#event.name == 'TEST_SENTRY_INTEGRATION'")
+	@Value("${test.sentry-on-container-start-up:false}")
+	private boolean testSentryExceptoin;
+
+	@PostConstruct
 	private void testSentryExceptoin(CustomSpringEvent event) throws ApplicationException {
-		try {
-			throw new Exception("Container restarted. And sentry logging is working fine.");
-		} catch (Exception e) {
-			log.error("Container restarted. And sentry logging is working fine.", e);
+		if (testSentryExceptoin) {
+			try {
+				throw new Exception("Container restarted. And sentry logging is working fine.");
+			} catch (Exception e) {
+				log.error("Container restarted. And sentry logging is working fine.", e);
+			}
 		}
 	}
 }
