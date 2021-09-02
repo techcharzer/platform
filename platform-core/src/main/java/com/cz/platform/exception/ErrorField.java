203abcdb9768dfbb5dd9a729c612265c7bc6fbdc
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-09-03 04:04:25 +0530

    no args constructore added.

--- platform-core/src/main/java/com/cz/platform/exception/ErrorField.java
+++ platform-core/src/main/java/com/cz/platform/exception/ErrorField.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorField implements Serializable {
 
 	private static final long serialVersionUID = 13234548716666666L;
