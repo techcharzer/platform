03679b629d1c9aea2aeec74df7bf7f60278c6edd
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-10-23 22:37:56 +0530

    unique utility updates

--- platform-core/src/main/java/com/cz/platform/utility/UniqueUtility.java
+++ platform-core/src/main/java/com/cz/platform/utility/UniqueUtility.java
@@ -15,12 +15,10 @@ import com.cz.platform.PlatformConstants;
 import com.cz.platform.exception.PlatformExceptionCodes;
 import com.cz.platform.exception.ValidationException;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@AllArgsConstructor
 public class UniqueUtility {
 
 	@Autowired
