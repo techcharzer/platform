fd9580de587a646498e65929efae2693113329c8
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-10-29 12:12:34 +0530

    charge mode bharat ac charger.

--- platform-core/src/main/java/com/cz/platform/enums/ChargerType.java
+++ platform-core/src/main/java/com/cz/platform/enums/ChargerType.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ChargerType {
-	KIRANA_CHARZER_GSM, KIRANA_CHARZER_BLE, OTHER_NETWORK_CHARGER, OCPP_16_JSON_CHARGER, KIRANA_CHARZER_FLEXTRON;
+	KIRANA_CHARZER_GSM, KIRANA_CHARZER_BLE, OTHER_NETWORK_CHARGER, OCPP_16_JSON_CHARGER, KIRANA_CHARZER_FLEXTRON, CHARGE_MOD_BHARAT_AC;
 
 	public static final Map<ChargerType, ConnectivityType> CHARGER_TYPE_TO_CONNECTIVITY = new HashMap<>();
 
@@ -20,6 +20,7 @@ public enum ChargerType {
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.OCPP_16_JSON_CHARGER, ConnectivityType.INTERNET);
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.KIRANA_CHARZER_GSM, ConnectivityType.INTERNET);
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.KIRANA_CHARZER_FLEXTRON, ConnectivityType.INTERNET);
+		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.CHARGE_MOD_BHARAT_AC, ConnectivityType.INTERNET);
 	}
 
 	public ConnectivityType getConnectivityType() {
