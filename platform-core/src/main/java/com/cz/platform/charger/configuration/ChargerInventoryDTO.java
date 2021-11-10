a63ea3aac200e6c1182ba0ee4b9a23b6625b4ab4
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-11-10 19:47:42 +0530

    configuration dto.

--- platform-core/src/main/java/com/cz/platform/charger/configuration/ChargerInventoryDTO.java
+++ platform-core/src/main/java/com/cz/platform/charger/configuration/ChargerInventoryDTO.java
@@ -3,24 +3,21 @@ package com.cz.platform.charger.configuration;
 import java.io.Serializable;
 
 import com.cz.platform.enums.ChargerType;
-import com.cz.platform.enums.ChargerUsageType;
 import com.cz.platform.enums.ConnectivityType;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import lombok.Data;
 
 @Data
-public class ChargerConfigurationDTO implements Serializable {
+public class ChargerInventoryDTO implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8410282833774514608L;
 	private String id;
 	private String uniqueIdentifier;
-	private ChargerUsageType usageType;
-	private String protectedNetworkId;
 	private ChargerType chargerType;
 	@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "chargerType")
 	@JsonSubTypes({ @Type(value = OCPP16ChargerConfiguration.class, name = "OCPP_16_JSON_CHARGER"),
@@ -28,6 +25,7 @@ public class ChargerConfigurationDTO implements Serializable {
 			@Type(value = KiranaChargerFlextronConfiguration.class, name = "KIRANA_CHARZER_FLEXTRON"),
 			@Type(value = KiranaChargerGsmConfiguration.class, name = "KIRANA_CHARZER_GSM") })
 	private ChargerConfiguration configuration;
+	private Boolean isActive;
 
 	public ConnectivityType getConnectivityType() {
 		return ChargerType.getConnectivityType(this.chargerType);
