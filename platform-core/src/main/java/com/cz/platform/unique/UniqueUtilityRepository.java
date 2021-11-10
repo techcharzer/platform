30e645f099a6f4f2891b4bf376957348892d9610
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-11-10 13:50:53 +0530

    fixes

--- platform-core/src/main/java/com/cz/platform/unique/UniqueUtilityRepository.java
+++ platform-core/src/main/java/com/cz/platform/unique/UniqueUtilityRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface UniqueUtilityRepository extends MongoRepository<UniqueUtilityEntity, String> {
+public interface UniqueUtilityRepository extends MongoRepository<UniqueUtilityEntity, String> {
 
 }
