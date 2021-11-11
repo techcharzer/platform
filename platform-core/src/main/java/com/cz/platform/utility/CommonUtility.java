d768f83fcf75e263f4ccb32fd79bc2c1fed5d9e0
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-11-11 14:38:28 +0530

    added criteria.

--- platform-core/src/main/java/com/cz/platform/utility/CommonUtility.java
+++ platform-core/src/main/java/com/cz/platform/utility/CommonUtility.java
@@ -1,8 +1,13 @@
 package com.cz.platform.utility;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.util.ObjectUtils;
 
 import com.cz.platform.dto.Range;
+import com.cz.platform.enums.ChargerType;
 
 public final class CommonUtility {
 
@@ -32,4 +37,42 @@ public final class CommonUtility {
 		}
 		return new Range<Long>(Long.parseLong(arr[0]), Long.parseLong(arr[1]));
 	}
+
+	public static Criteria chargerControlIdCriteria(String chargerTypeKey, String keyOfConfiguration,
+			String chargerControlId) {
+		Criteria criteria = new Criteria();
+		List<Criteria> list = new ArrayList<>();
+		for (ChargerType val : ChargerType.values()) {
+			Criteria inCriteria = new Criteria();
+			Criteria valCriteria = null;
+			switch (val) {
+			case KIRANA_CHARZER_BLE:
+				valCriteria = Criteria.where(keyOfConfiguration.concat(".macAddress")).is(chargerControlId);
+				break;
+			case KIRANA_CHARZER_GSM:
+				valCriteria = Criteria.where(keyOfConfiguration.concat(".deviceId")).is(chargerControlId);
+				break;
+			case KIRANA_CHARZER_FLEXTRON:
+				valCriteria = Criteria.where(keyOfConfiguration.concat(".ccuId")).is(chargerControlId);
+				break;
+			case CHARGE_MOD_BHARAT_AC:
+				break;
+			case OCPP_16_JSON_CHARGER:
+				valCriteria = Criteria.where(keyOfConfiguration.concat(".chargerBoxId")).is(chargerControlId);
+				break;
+			case OTHER_NETWORK_CHARGER:
+				break;
+			default:
+				break;
+			}
+			if (!ObjectUtils.isEmpty(valCriteria)) {
+				Criteria chargerType = Criteria.where(chargerTypeKey).is(val.name());
+				list.add(inCriteria.andOperator(chargerType, valCriteria));
+			}
+		}
+		Criteria[] array = list.toArray(new Criteria[list.size()]);
+		criteria.orOperator(array);
+		return criteria;
+	}
+
 }
