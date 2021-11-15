3c0a684b141d811a62d3f8a92e50bbd56fa2aecb
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-11-15 18:05:21 +0530

    removed extra content.

--- platform-core/src/main/java/com/cz/platform/filters/FilterToQueryMapperFactory.java
+++ platform-core/src/main/java/com/cz/platform/filters/FilterToQueryMapperFactory.java
@@ -1,8 +1,5 @@
 package com.cz.platform.filters;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public abstract class FilterToQueryMapperFactory {
 
 	public abstract GenericFilterToQueryMapper getService(Class a);
