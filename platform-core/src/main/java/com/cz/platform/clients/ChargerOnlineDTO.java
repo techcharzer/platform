e219c477d6599965410eb9686beebbf97c854464
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-11-25 13:15:23 +0530

    set the value.

--- platform-core/src/main/java/com/cz/platform/clients/ChargerOnlineDTO.java
+++ platform-core/src/main/java/com/cz/platform/clients/ChargerOnlineDTO.java
@@ -2,12 +2,47 @@ package com.cz.platform.clients;
 
 import java.time.Instant;
 
-import lombok.Data;
+import org.apache.commons.lang3.BooleanUtils;
 
-@Data
 public class ChargerOnlineDTO {
 	private Boolean isOnline = false;
 	private Instant lastSeen;
 	private Long consumptionRateInWattHour;
 	private Long unitsConsumedInWattHour;
+
+	public Boolean getIsOnline() {
+		return isOnline;
+	}
+
+	public void setIsOnline(Boolean isOnline) {
+		this.isOnline = isOnline;
+	}
+
+	public Instant getLastSeen() {
+		return lastSeen;
+	}
+
+	public void setLastSeen(Instant lastSeen) {
+		this.lastSeen = lastSeen;
+	}
+
+	public Long getConsumptionRateInWattHour() {
+		return consumptionRateInWattHour;
+	}
+
+	public void setConsumptionRateInWattHour(Long consumptionRateInWattHour) {
+		this.consumptionRateInWattHour = consumptionRateInWattHour;
+	}
+
+	public Long getUnitsConsumedInWattHour() {
+		if (BooleanUtils.isTrue(isOnline)) {
+			return unitsConsumedInWattHour;
+		} else {
+			return 0L;
+		}
+	}
+
+	public void setUnitsConsumedInWattHour(Long unitsConsumedInWattHour) {
+		this.unitsConsumedInWattHour = unitsConsumedInWattHour;
+	}
 }
