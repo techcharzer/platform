a8aab56f55d2e67debc41fe2a643bf1346fe439f
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2021-11-26 15:55:53 +0530

    added keys

--- platform-core/src/main/java/com/cz/platform/dto/OtherNetworkDealConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/dto/OtherNetworkDealConfiguration.java
@@ -5,8 +5,9 @@ import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class OtherNetworkDealConfiguration extends DealConfigurationData implements Serializable {/**
-	 * 
-	 */
+public class OtherNetworkDealConfiguration extends DealConfigurationData implements Serializable {
+	/**
+	* 
+	*/
 	private static final long serialVersionUID = -5272882314725746192L;
 }
