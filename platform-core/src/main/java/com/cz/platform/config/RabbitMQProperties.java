00aa6125da040731e5b04578d3def74ee82a0730
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-01-23 18:08:22 +0530

    list

--- platform-core/src/main/java/com/cz/platform/config/RabbitMQProperties.java
+++ platform-core/src/main/java/com/cz/platform/config/RabbitMQProperties.java
@@ -1,11 +1,15 @@
 package com.cz.platform.config;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 
 import lombok.Data;
 
@@ -20,5 +24,21 @@ public class RabbitMQProperties {
 	private Integer corePoolSize = 3;
 	private Integer maxPoolSize = 6;
 	private Integer poolQueueCapacity = 50;
+
+	private static Map<String, QueueConfiguration> QUEUE_NAME_MAP;
+
+	public Map<String, QueueConfiguration> getQueueNameConfigMap() {
+		if (ObjectUtils.isEmpty(QUEUE_NAME_MAP)) {
+			Map<String, QueueConfiguration> internal = new HashMap<>();
+			if (!ObjectUtils.isEmpty(queueConfiguration)) {
+				for (QueueConfiguration config : queueConfiguration) {
+					internal.put(config.getQueueName(), config);
+				}
+			}
+			QUEUE_NAME_MAP = Collections.unmodifiableMap(internal);
+		}
+		return QUEUE_NAME_MAP;
+
+	}
 
 }
