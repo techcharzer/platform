2a2690cbdd5ad864f0ed0c1c184c0e2081982473
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-01-24 19:48:34 +0530

    added the device id and ev point charger configuration.

--- platform-core/src/main/java/com/cz/platform/enums/ChargerType.java
+++ platform-core/src/main/java/com/cz/platform/enums/ChargerType.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public enum ChargerType {
 	KIRANA_CHARZER_GSM, KIRANA_CHARZER_BLE, OTHER_NETWORK_CHARGER, OCPP_16_JSON_CHARGER, KIRANA_CHARZER_FLEXTRON,
-	CHARGE_MOD_BHARAT_AC;
+	CHARGE_MOD_BHARAT_AC, EV_POINT_CHARGER;
 
 	public static final Map<ChargerType, ConnectivityType> CHARGER_TYPE_TO_CONNECTIVITY = new HashMap<>();
 
@@ -22,6 +22,7 @@ public enum ChargerType {
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.KIRANA_CHARZER_GSM, ConnectivityType.INTERNET);
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.KIRANA_CHARZER_FLEXTRON, ConnectivityType.INTERNET);
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.CHARGE_MOD_BHARAT_AC, ConnectivityType.INTERNET);
+		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.EV_POINT_CHARGER, ConnectivityType.INTERNET);
 	}
 
 	public ConnectivityType getConnectivityType() {
