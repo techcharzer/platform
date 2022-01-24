69ada72a4ff0c31fc960bca2a374211c53a43d81
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-01-24 23:02:56 +0530

    socket id.

--- platform-core/src/main/java/com/cz/platform/clients/HardwareStatusInfo.java
+++ platform-core/src/main/java/com/cz/platform/clients/HardwareStatusInfo.java
@@ -1,7 +1,5 @@
 package com.cz.platform.clients;
 
-import java.util.Map;
-
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.ObjectUtils;
 
@@ -10,22 +8,17 @@ import lombok.Data;
 @Data
 public class HardwareStatusInfo {
 	private String hardwareId;
-	private Map<String, SocketStatusInfo> socket;
+	private String socketId;
+	private ChargerOnlineDTO onlineDTO;
+	private Boolean relay;
+	private Long ecInmA;
+	private Long cmrInWH;
 
-	@Data
-	public static class SocketStatusInfo {
-		private String socketId;
-		private ChargerOnlineDTO onlineDTO;
-		private Boolean relay;
-		private Long ecInmA;
-		private Long cmrInWH;
-
-		public Long getEcInmA() {
-			if (!ObjectUtils.isEmpty(onlineDTO) && BooleanUtils.isTrue(onlineDTO.getIsOnline())) {
-				return ecInmA;
-			} else {
-				return 0L;
-			}
+	public Long getEcInmA() {
+		if (!ObjectUtils.isEmpty(onlineDTO) && BooleanUtils.isTrue(onlineDTO.getIsOnline())) {
+			return ecInmA;
+		} else {
+			return 0L;
 		}
 	}
 
