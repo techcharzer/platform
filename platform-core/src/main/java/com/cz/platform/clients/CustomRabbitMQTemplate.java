0406c2b91bb3eb670d7dd21373235ff2ed88c0ef
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-01-25 20:44:54 +0530

    fixed the issue.

--- platform-core/src/main/java/com/cz/platform/clients/CustomRabbitMQTemplate.java
+++ platform-core/src/main/java/com/cz/platform/clients/CustomRabbitMQTemplate.java
@@ -5,8 +5,11 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import com.cz.platform.config.QueueConfiguration;
+import com.cz.platform.exception.ApplicationException;
 import com.cz.platform.exception.PlatformExceptionCodes;
 import com.cz.platform.exception.ValidationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class CustomRabbitMQTemplate {
 	private RabbitTemplate template;
+	private ObjectMapper mapper;
 
 	public void convertAndSend(QueueConfiguration qConfig, Object data) {
-		log.info("messgae {} queued to: {}", data, qConfig);
 		if (ObjectUtils.isEmpty(qConfig)) {
 			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(), "Invalid queue configuration");
 		}
@@ -31,6 +34,14 @@ public class CustomRabbitMQTemplate {
 		if (ObjectUtils.isEmpty(data)) {
 			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(), "Invalid data");
 		}
-		template.convertAndSend(qConfig.getExchangeName(), qConfig.getRoutingKey(), data);
+		try {
+			String jsonString = mapper.writeValueAsString(data);
+			log.info("messgae {} queued to: {}", jsonString, qConfig);
+			template.convertAndSend(qConfig.getExchangeName(), qConfig.getRoutingKey(), jsonString);
+		} catch (JsonProcessingException e) {
+			log.error("error occured while converting to json the data : {}", data, e);
+			throw new ApplicationException(PlatformExceptionCodes.INTERNAL_SERVER_ERROR, e);
+		}
+
 	}
 }
