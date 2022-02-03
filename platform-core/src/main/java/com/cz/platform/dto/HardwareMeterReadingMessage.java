1de0b619ac6d394b1ad53ef63907b67c3b1ba2b8
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-02-03 13:03:40 +0530

    fixed.

--- platform-core/src/main/java/com/cz/platform/dto/HardwareMeterReadingMessage.java
+++ platform-core/src/main/java/com/cz/platform/dto/HardwareMeterReadingMessage.java
@@ -1,11 +1,14 @@
 package com.cz.platform.dto;
 
+import com.cz.platform.enums.ChargerType;
+
 import lombok.Data;
 
 @Data
 public class HardwareMeterReadingMessage {
 	private String bookingId;
 	private String hardwareId;
+	private ChargerType chargerType;
 	private Long cmrInWH;
 	private String socketId;
 	private Long lastNPingSameCount;
