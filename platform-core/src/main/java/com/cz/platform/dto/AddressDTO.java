cc096abfd05fc0e2d6a66b50674154a620ed547a
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-02-10 01:06:07 +0530

    converted the id to string.

--- platform-core/src/main/java/com/cz/platform/dto/AddressDTO.java
+++ platform-core/src/main/java/com/cz/platform/dto/AddressDTO.java
@@ -13,6 +13,6 @@ public class AddressDTO implements Serializable {
 	private static final long serialVersionUID = -3297786397246929480L;
 	private GeoCoordinatesDTO coordinates;
 	private String address;
-	private Long cityId;
+	private String cityId;
 	private String pinCode;
 }
