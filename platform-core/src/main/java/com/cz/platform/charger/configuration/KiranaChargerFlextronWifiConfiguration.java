ea718be2520fcc6c52f8d75269b1d3fe3873e6f9
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-02-17 22:36:33 +0530

    fixed.

---
+++ platform-core/src/main/java/com/cz/platform/charger/configuration/KiranaChargerFlextronWifiConfiguration.java
@@ -0,0 +1,21 @@
+package com.cz.platform.charger.configuration;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class KiranaChargerFlextronWifiConfiguration implements HardwareConfigurationData, Serializable {
+
+	/**
+	 * ccuId mqttBrokerKey
+	 */
+	private static final long serialVersionUID = 1839211805479707179L;
+	private String ccuId;
+	private String mqttBrokerKey;
+
+	@Override
+	public String getChargerControlId() {
+		return ccuId;
+	}
+}
