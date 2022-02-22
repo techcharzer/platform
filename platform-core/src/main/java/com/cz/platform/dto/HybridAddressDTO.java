93746471b585be1712c4099d74a1029458b9a212
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-02-22 17:48:35 +0530

    fixed.

--- platform-core/src/main/java/com/cz/platform/dto/HybridAddressDTO.java
+++ platform-core/src/main/java/com/cz/platform/dto/HybridAddressDTO.java
@@ -13,6 +13,5 @@ public class HybridAddressDTO implements Serializable, AddressData {
 	private static final long serialVersionUID = -3297786397246929480L;
 	private GeoCoordinatesDTO coordinates;
 	private String postalAddress;
-	private String cityId;
 	private String pinCode;
 }
