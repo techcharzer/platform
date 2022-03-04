0093c35ed767258baadda973f20bd1d5972c166e
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-03-04 15:54:00 +0530

    added more deails.

--- platform-core/src/main/java/com/cz/platform/dto/order/NetworkBuyersOrderConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/dto/order/NetworkBuyersOrderConfiguration.java
@@ -1,8 +1,12 @@
 package com.cz.platform.dto.order;
 
+import com.cz.platform.dto.AddressDTOV2;
+
 import lombok.Data;
 
 @Data
 public class NetworkBuyersOrderConfiguration implements OrderConfiguration {
-	
+	private AddressDTOV2 address;
+	private String bussinessName;
+	private Double networkBuyerShare;
 }
