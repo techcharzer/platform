50acb4999d13cdc2550444e810f65f5d36c6fca9
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-03-07 08:48:49 +0530

    fixed.

--- platform-core/src/main/java/com/cz/platform/config/RestTemplateConfig.java
+++ platform-core/src/main/java/com/cz/platform/config/RestTemplateConfig.java
@@ -1,15 +1,21 @@
 package com.cz.platform.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.cz.platform.PlatformConstants;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RestTemplateConfig {
+
+	@Autowired
+	private ObjectMapper mapper;
 
 	@Bean
 	@Primary
@@ -22,7 +28,13 @@ public class RestTemplateConfig {
 		httpRequestFactory.setConnectionRequestTimeout(timeout);
 		httpRequestFactory.setConnectTimeout(timeout);
 		httpRequestFactory.setReadTimeout(timeout);
-		return new RestTemplate(httpRequestFactory);
+
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setObjectMapper(mapper);
+
+		RestTemplate template = new RestTemplate(httpRequestFactory);
+		template.getMessageConverters().add(0, converter);
+		return template;
 	}
 
 	@Bean(PlatformConstants.EXTERNAL_CLIENT)
