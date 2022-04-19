e04a7cc258f184b6704a0b88763dcc9ebb30ecd3
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-04-19 17:22:40 +0530

    fixed.

--- platform-core/src/main/java/com/cz/platform/dto/UserDetails.java
+++ platform-core/src/main/java/com/cz/platform/dto/UserDetails.java
@@ -1,9 +1,15 @@
 package com.cz.platform.dto;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class UserDetails {
+public class UserDetails implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8315971748398515838L;
 	private String userId;
 	private String mobileNumber;
 	private String name;
