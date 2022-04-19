9c03d8259209049db7924b17698ff8eb3fef61b0
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-04-19 13:08:40 +0530

    added

--- platform-core/src/main/java/com/cz/platform/exception/PlatformExceptionCodes.java
+++ platform-core/src/main/java/com/cz/platform/exception/PlatformExceptionCodes.java
@@ -10,7 +10,8 @@ public enum PlatformExceptionCodes implements IExceptionCodes {
 	NOT_FOUND("PL_404", "Data not found"), INVALID_DATA("PL_1000", "Invalid/empty data requested"),
 	INTERNAL_SERVER_ERROR("PL_1001", "Some Internal Server Error occured"),
 	SERVICE_NOT_WORKING("PL_1002", "Some Internal dependent service not working"),
-	AUTHENTICATION_CODE("PL_1003", "Authentication failure"), ACCESS_DENIED("PL_1004", "Authorization failure");
+	AUTHENTICATION_CODE("PL_1003", "Authentication failure"), ACCESS_DENIED("PL_1004", "Authorization failure"),
+	UPDATE_APP_TO_LATEST_VERSION("PL_500", "Update the application to latest version");
 
 	private String code;
 	private String message;
