176f73b963771cf6b5d707bf11f8a3cd5cdd1ecf
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-06-07 14:55:51 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/enums/ChargerType.java
+++ platform-core/src/main/java/com/cz/platform/enums/ChargerType.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public enum ChargerType {
 	KIRANA_CHARZER_GSM, KIRANA_CHARZER_BLE, OTHER_NETWORK_CHARGER, OCPP_16_JSON_CHARGER, KIRANA_CHARZER_FLEXTRON,
-	CHARGE_MOD_BHARAT_AC, EV_POINT_CHARGER, KIRANA_CHARZER_FLEXTRON_WIFI;
+	CHARGE_MOD_BHARAT_AC, EV_POINT_CHARGER, KIRANA_CHARZER_FLEXTRON_WIFI, FLEXTRON_BLE_OMNI;
 
 	public static final Map<ChargerType, ConnectivityType> CHARGER_TYPE_TO_CONNECTIVITY = new HashMap<>();
 
@@ -16,6 +16,7 @@ public enum ChargerType {
 	private static void fillConnectivityTypeForAllChargerTypes() {
 		// BLE
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.KIRANA_CHARZER_BLE, ConnectivityType.BLUETOOTH);
+		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.FLEXTRON_BLE_OMNI, ConnectivityType.BLUETOOTH);
 
 		// GSM
 		CHARGER_TYPE_TO_CONNECTIVITY.put(ChargerType.OCPP_16_JSON_CHARGER, ConnectivityType.INTERNET);
