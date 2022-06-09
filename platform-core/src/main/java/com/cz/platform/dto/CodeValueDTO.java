0a60533cee19f003c6c352806c9605b17d9cd7b1
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-06-09 15:11:48 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/dto/CodeValueDTO.java
+++ platform-core/src/main/java/com/cz/platform/dto/CodeValueDTO.java
@@ -1,11 +1,17 @@
 package com.cz.platform.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class CodeValueDTO<T, U> {
+public class CodeValueDTO<T, U> implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2603846881117825438L;
 	private T code;
 	private U value;
 }
