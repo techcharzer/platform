f6cec1e468c14251880c8c32ba77660fb5761fbd
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-07-26 03:17:16 +0530

    in case of premiseOwner the profitSharPercentage is not defined.

--- platform-core/src/main/java/com/cz/platform/dto/PremiseOwnerHostConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/dto/PremiseOwnerHostConfiguration.java
@@ -1,8 +1,6 @@
 package com.cz.platform.dto;
 
 import com.cz.platform.enums.HostTypeEnum;
-import com.cz.platform.exception.PlatformExceptionCodes;
-import com.cz.platform.exception.ValidationException;
 
 import lombok.Data;
 
@@ -12,8 +10,7 @@ public class PremiseOwnerHostConfiguration implements HostConfiguration {
 
 	@Override
 	public Double getProfitSharePercentageOnBooking() {
-		throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(),
-				"User is not a dealer. Please ask charzer team to make this user as dealer.");
+		return null;
 	}
 
 }
