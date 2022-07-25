1f3b4194ea99af8c20b933fc41a3dc1b3834e542
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-07-26 00:18:58 +0530

    added the validation.

--- platform-core/src/main/java/com/cz/platform/dto/PremiseOwnerHostConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/dto/PremiseOwnerHostConfiguration.java
@@ -1,10 +1,19 @@
 package com.cz.platform.dto;
 
 import com.cz.platform.enums.HostTypeEnum;
+import com.cz.platform.exception.PlatformExceptionCodes;
+import com.cz.platform.exception.ValidationException;
 
 import lombok.Data;
 
 @Data
 public class PremiseOwnerHostConfiguration implements HostConfiguration {
 	private HostTypeEnum type = HostTypeEnum.PREMISE_OWNER;
+
+	@Override
+	public Double getProfitSharePercentageOnBooking() {
+		throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(),
+				"User is not a dealer. Please ask charzer team to make this user as dealer.");
+	}
+
 }
