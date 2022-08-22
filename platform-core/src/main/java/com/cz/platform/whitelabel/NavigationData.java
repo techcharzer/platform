c2a65dfd2e9ef1241107fe9ae5df4d37a7f42192
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2022-08-22 13:42:36 +0530

    added open in

--- platform-core/src/main/java/com/cz/platform/whitelabel/NavigationData.java
+++ platform-core/src/main/java/com/cz/platform/whitelabel/NavigationData.java
@@ -8,5 +8,10 @@ import lombok.Data;
 @Data
 public class NavigationData {
 	private String to;
+	private OpenIn openIn;
 	private Map<String, String> metaData = new HashMap<>();
+
+	public static enum OpenIn {
+		MOBILE_APP, BROWSER
+	}
 }
