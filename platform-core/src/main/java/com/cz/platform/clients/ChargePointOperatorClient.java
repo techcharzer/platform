4d9f1abd5f034624bc581440e0f06891cdeb3976
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2023-03-16 02:11:23 +0530

    added api to fetch all the charge point operators

--- platform-core/src/main/java/com/cz/platform/clients/ChargePointOperatorClient.java
+++ platform-core/src/main/java/com/cz/platform/clients/ChargePointOperatorClient.java
@@ -14,13 +14,11 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import com.cz.platform.PlatformConstants;
-import com.cz.platform.dto.AddressDTO;
 import com.cz.platform.exception.ApplicationException;
 import com.cz.platform.exception.PlatformExceptionCodes;
 import com.cz.platform.exception.ValidationException;
 import com.cz.platform.security.SecurityConfigProps;
 import com.cz.platform.utility.PlatformCommonService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,8 +34,6 @@ public class ChargePointOperatorClient {
 	private UrlConfig urlConfig;
 
 	private SecurityConfigProps securityProps;
-
-	private ObjectMapper mapper;
 
 	private PlatformCommonService commonService;
 
@@ -81,6 +77,27 @@ public class ChargePointOperatorClient {
 		}
 	}
 
+	public ChargePointOperatorDTO[] getChargePointOperators() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get("user-service"));
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+		try {
+			String url = MessageFormat.format("{0}/user-service/secure/internal-server/chargePointOperator",
+					urlConfig.getBaseUrl());
+			log.debug("request: {}, headers {}", url, entity);
+			ResponseEntity<ChargePointOperatorDTO[]> response = template.exchange(url, HttpMethod.GET, entity,
+					ChargePointOperatorDTO[].class);
+			log.info("api response : {}", response);
+			return response.getBody();
+		} catch (HttpStatusCodeException exception) {
+			log.error("error response from the server :{}", exception.getResponseBodyAsString());
+			throw new ApplicationException(PlatformExceptionCodes.INTERNAL_SERVER_ERROR.getCode(),
+					"charge point api not working");
+		}
+	}
+
 	@Data
 	public static class ChargePointOperatorDTO implements Serializable {
 		/**
@@ -89,9 +106,6 @@ public class ChargePointOperatorClient {
 		private static final long serialVersionUID = 6106129986682393403L;
 		private String id;
 		private String companyName;
-		private String website;
-		private String gstNumber;
-		private AddressDTO address;
 	}
 
 }
