47fb57a24ee6c7fa0d64b991b6de7c4adc5d353c
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2023-06-21 13:27:11 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/filters/GenericDTOFilterServiceFactory.java
+++ platform-core/src/main/java/com/cz/platform/filters/GenericDTOFilterServiceFactory.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public abstract class GenericDTOFilterServiceFactory {
+@Service
+public class GenericDTOFilterServiceFactory {
 
 	protected final Map<Class, DTOFilterService> MAP_CLASS_TO_FILTER_QUERY_CREATOR = new HashMap<>();
-	
-	protected abstract void fillMap();
+
+	protected void fillMap() {
+	}
 
 	public DTOFilterService getService(Class a) {
 		if (ObjectUtils.isEmpty(a)) {
