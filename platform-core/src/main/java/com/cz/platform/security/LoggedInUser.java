1afd86d26b0f57fc449a6ab64619fad9b8cf6d19
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2023-06-30 14:47:00 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/security/LoggedInUser.java
+++ platform-core/src/main/java/com/cz/platform/security/LoggedInUser.java
@@ -3,13 +3,11 @@ package com.cz.platform.security;
 import java.time.ZoneId;
 import java.util.List;
 
-import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.ObjectUtils;
 
 import com.cz.platform.PlatformConstants;
 import com.cz.platform.enums.OperatingSystem;
 import com.cz.platform.enums.UserType;
-import com.cz.platform.exception.AuthenticationException;
-import com.cz.platform.exception.PlatformExceptionCodes;
 
 import lombok.Data;
 
@@ -19,6 +17,7 @@ class LoggedInUser implements UserDTO {
 	private String userId;
 	private String mobileNumber;
 	private List<RoleDTO> roles;
+	// this fields is equivalent to login from.
 	private UserType userType;
 	private String chargePointOperatorId;
 	private OperatingSystem operatingSystem;
@@ -29,16 +28,8 @@ class LoggedInUser implements UserDTO {
 	}
 
 	@Override
-	public boolean hasCZOAccess() {
-		return UserType.CZO.equals(this.userType);
-	}
-
-	@Override
-	public void validateCZOAccess() {
-		if (BooleanUtils.isFalse(hasCZOAccess())) {
-			throw new AuthenticationException(PlatformExceptionCodes.ACCESS_DENIED.getCode(),
-					"Does not have CZO access");
-		}
+	public boolean isLogInFrom(UserType logInFrom) {
+		return ObjectUtils.isEmpty(logInFrom) ? false : logInFrom.equals(this.userType);
 	}
 
 }
--- platform-core/src/main/java/com/cz/platform/security/UserDTO.java
+++ platform-core/src/main/java/com/cz/platform/security/UserDTO.java
@@ -1,10 +1,16 @@
 package com.cz.platform.security;
 
+import java.text.MessageFormat;
 import java.time.ZoneId;
 import java.util.List;
 
-import com.cz.platform.enums.OperatingSystem;
+import org.apache.commons.lang3.BooleanUtils;
+
 import com.cz.platform.enums.UserType;
+import com.cz.platform.enums.OperatingSystem;
+import com.cz.platform.exception.AuthenticationException;
+import com.cz.platform.exception.PlatformExceptionCodes;
+import com.cz.platform.exception.ValidationException;
 
 public interface UserDTO {
 	public String getUserId();
@@ -15,11 +21,21 @@ public interface UserDTO {
 
 	public ZoneId getZoneId();
 
-	public UserType getUserType();
+	public boolean isLogInFrom(UserType logInFrom);
 
-	public boolean hasCZOAccess();
+	public default void validateLogInFrom(UserType logInFrom) {
+		if (BooleanUtils.isFalse(isLogInFrom(logInFrom))) {
+			String message = MessageFormat.format("User has not logged from : {0}", logInFrom.name());
+			throw new ValidationException(PlatformExceptionCodes.ACCESS_DENIED.getCode(), message);
+		}
+	}
 
-	public void validateCZOAccess();
+	public default void validateCZOAccess() {
+		if (BooleanUtils.isFalse(isLogInFrom(UserType.CZO))) {
+			throw new ValidationException(PlatformExceptionCodes.ACCESS_DENIED.getCode(),
+					"Does not have CZO access");
+		}
+	}
 
 	public String getChargePointOperatorId();
 
