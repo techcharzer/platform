45df5ac09627e9d1c70a00d86bcbaf87dfbd8604
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2023-09-26 14:51:20 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/dashboard/DashboardController.java
+++ platform-core/src/main/java/com/cz/platform/dashboard/DashboardController.java
@@ -2,13 +2,15 @@ package com.cz.platform.dashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cz.platform.dashboard.GenericDashBoardService.IFetchDashboardRequest;
 import com.cz.platform.security.SecurityUtils;
 import com.cz.platform.security.UserDTO;
+
+import lombok.Data;
 
 @RestController
 public class DashboardController {
@@ -20,14 +22,73 @@ public class DashboardController {
 	@GetMapping("/secure/dashboard/app/{key}")
 	public DashboardCardDTO getDashBoardCardDTOForAPP(@PathVariable("key") String key) {
 		UserDTO user = SecurityUtils.getLoggedInUser();
-		return filterService.getDashBoardCardDTO(key, user.getUserId());
+		DHAppDashboardRequest request = new DHAppDashboardRequest();
+		request.setKey(key);
+		request.setUserId(user.getUserId());
+		return filterService.getDashBoardCardDTO(request);
+	}
+
+	@Data
+	public static class DHAppDashboardRequest implements IFetchDashboardRequest {
+		private String key;
+		private String userId;
 	}
 
 	@GetMapping("/secure/dashboard/czo/{key}")
-	@Secured("ROLE_EXECUTE_DASHBOARD")
 	public DashboardCardDTO getDashBoardCardDTOForCZO(@PathVariable("key") String key) {
+		CZODashboardRequest request = new CZODashboardRequest();
+		request.setKey(key);
+		return filterService.getDashBoardCardDTO(request);
+	}
+
+	@Data
+	public static class CZODashboardRequest implements IFetchDashboardRequest {
+		private String key;
+	}
+
+	@GetMapping("/secure/dashboard/cms/{key}")
+	public DashboardCardDTO getDashBoardCardDTOForCMS(@PathVariable("key") String key) {
 		UserDTO user = SecurityUtils.getLoggedInUser();
-		return filterService.getDashBoardCardDTO(key, user.getUserId());
+		CMSDashboardRequest request = new CMSDashboardRequest();
+		request.setKey(key);
+		request.setChargePointOperatorId(user.getChargePointOperatorId());
+		return filterService.getDashBoardCardDTO(request);
+	}
+
+	@Data
+	public static class CMSDashboardRequest implements IFetchDashboardRequest {
+		private String key;
+		private String chargePointOperatorId;
+	}
+
+	@GetMapping("/secure/dashboard/group/{groupId}/{key}")
+	public DashboardCardDTO getDashBoardCardDTOForUserGroup(@PathVariable("key") String key,
+			@PathVariable("groupId") String groupId) {
+		UserGroupDashboardRequest request = new UserGroupDashboardRequest();
+		request.setKey(key);
+		request.setGroupId(groupId);
+		return filterService.getDashBoardCardDTO(request);
+	}
+
+	@Data
+	public static class UserGroupDashboardRequest implements IFetchDashboardRequest {
+		private String key;
+		private String groupId;
+	}
+
+	@GetMapping("/secure/dashboard/fleet/{fleetId}/{key}")
+	public DashboardCardDTO getDashBoardCardDTOForFleet(@PathVariable("key") String key,
+			@PathVariable("fleetId") String fleetId) {
+		FleetDashboardRequest request = new FleetDashboardRequest();
+		request.setKey(key);
+		request.setFleetId(fleetId);
+		return filterService.getDashBoardCardDTO(request);
+	}
+
+	@Data
+	public static class FleetDashboardRequest implements IFetchDashboardRequest {
+		private String key;
+		private String fleetId;
 	}
 
 }
--- platform-core/src/main/java/com/cz/platform/dashboard/GenericDashBoardService.java
+++ platform-core/src/main/java/com/cz/platform/dashboard/GenericDashBoardService.java
@@ -3,6 +3,8 @@ package com.cz.platform.dashboard;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 import com.cz.platform.exception.ApplicationException;
 import com.cz.platform.exception.PlatformExceptionCodes;
 import com.cz.platform.exception.ValidationException;
@@ -16,20 +18,30 @@ public class GenericDashBoardService {
 
 	@FunctionalInterface
 	public interface CustomDasboardCardFetcher {
-		DashboardCardDTO fetchData(String userId);
+		DashboardCardDTO fetchData(IFetchDashboardRequest request);
 	}
 
-	public DashboardCardDTO getDashBoardCardDTO(String key, String userId) {
-		if (!MAP_OF_DATA_FETCHERS.containsKey(key)) {
+	public DashboardCardDTO getDashBoardCardDTO(IFetchDashboardRequest request) {
+		if (ObjectUtils.isEmpty(request)) {
+			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(), "Invalid request");
+		}
+		if (ObjectUtils.isEmpty(request.getKey())) {
+			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(), "Invalid key");
+		}
+		if (!MAP_OF_DATA_FETCHERS.containsKey(request.getKey())) {
 			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(),
 					"Invalid key for fetching dashboard");
 		}
 		try {
-			return MAP_OF_DATA_FETCHERS.get(key).fetchData(userId);
+			return MAP_OF_DATA_FETCHERS.get(request.getKey()).fetchData(request);
 		} catch (Exception e) {
-			log.error("error occured while fetching the dashboard: {}", key, e);
+			log.error("error occured while fetching the dashboard: {}", request, e);
 			throw new ApplicationException(PlatformExceptionCodes.INVALID_DATA.getCode(), e.getMessage());
 		}
+	}
+
+	public static interface IFetchDashboardRequest {
+		String getKey();
 	}
 
 }
