d843c07e56de8462609798a71cfffa0def8f0390
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2023-12-18 16:58:08 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/dto/ProfitShareConfiguration.java
+++ platform-core/src/main/java/com/cz/platform/dto/ProfitShareConfiguration.java
@@ -12,6 +12,7 @@ public class ProfitShareConfiguration implements Serializable {
 	private static final long serialVersionUID = -21090774840692732L;
 	private String chargePointOperatorId;
 	private Double charzerShare;
+	private Double chargePointOperatorShare;
 	private UserShare dealerShare;
 	private UserShare secondaryDealerShare;
 	private UserShare tertiaryDealerShare;
