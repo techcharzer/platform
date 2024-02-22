315143906fe5c987662188ff4f1c2f216a237a06
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2024-02-22 22:57:21 +0530

    fixed

--- platform-core/src/main/java/com/cz/platform/security/ActuatorSecurityConfig.java
+++ platform-core/src/main/java/com/cz/platform/security/ActuatorSecurityConfig.java
@@ -11,7 +11,6 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/actuator/**").hasRole("ACTUATOR_ENDPOINTS").antMatchers("/**")
-				.permitAll();
+		http.authorizeRequests().antMatchers("/actuator/**").hasRole("ACTUATOR_ENDPOINTS").anyRequest().authenticated();
 	}
 }
