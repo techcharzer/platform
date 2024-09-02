fd17921006a5c15ae96a19778a009dcf92eec988
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2024-09-02 15:48:15 +0530

    addedsociety configuration

--- platform-core/src/main/java/com/cz/platform/dto/Society.java
+++ platform-core/src/main/java/com/cz/platform/dto/Society.java
@@ -14,10 +14,30 @@ public class Society implements GroupConfiguration, Serializable {
 	private String defaultImageUrl;
 	private String registrationDeeplink;
 	private Double serviceChargePercentage;
+	private SocietyConfiguration configuration;
 
 	@Override
 	public String getName() {
 		return societyName;
+	}
+
+	@Data
+	public static class SocietyConfiguration implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -3173206499176485600L;
+		private UtilizationLimit utilizationLimit;
+	}
+
+	@Data
+	public static class UtilizationLimit implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -7200456165935601236L;
+		private Boolean isApplied;
+		private Long value;
 	}
 
 }
