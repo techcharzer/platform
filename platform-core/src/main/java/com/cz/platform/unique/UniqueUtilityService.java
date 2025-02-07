12f41f30f3f4a29d7b7aca8e0fc62c83a60f443f
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2025-02-08 00:00:45 +0530

    fixed multithreading race condition issue.

--- platform-core/src/main/java/com/cz/platform/unique/UniqueUtilityService.java
+++ platform-core/src/main/java/com/cz/platform/unique/UniqueUtilityService.java
@@ -4,24 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-import org.redisson.api.RAtomicLong;
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.data.redis.core.BoundHashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
-import com.cz.platform.PlatformConstants;
-import com.cz.platform.exception.LoggerType;
 import com.cz.platform.exception.PlatformExceptionCodes;
 import com.cz.platform.exception.ValidationException;
-import com.cz.platform.utility.PlatformCommonService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,15 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 public class UniqueUtilityService {
 
 	@Autowired
-	@Qualifier(PlatformConstants.REDIS_TEMPLATE_FOR_UNIQUE_NUMBERS)
-	private RedisTemplate<String, Integer> redisTemplate;
-	private static final String NEW_LOGIC_KEY = "NEW_LOGIC_KEY_V1";
-	@Autowired
-	private RedissonClient redissonClient;
-	@Autowired
 	private MongoTemplate mongoTemplate;
-	@Autowired
-	private PlatformCommonService platformCommonService;
 	private static final String BASE_SALT = "23456789abcdefghijkmnpqrstuvwxyz";
 	private static final Map<Long, Character> MAP_OF_INTEGER_TO_CHARACTER = new HashMap<>();
 
@@ -57,34 +42,26 @@ public class UniqueUtilityService {
 	}
 
 	public String getUniqueId(String basePath) {
-		Long val = getNextNumber(basePath, true, random.nextInt(100));
+		Long val = getNextNumber(basePath, random.nextInt(100));
 		return getString(val);
 	}
 
 	public String getNextId(String basePath) {
-		Long val = getNextNumber(basePath, true, 1);
+		Long val = getNextNumber(basePath, 1);
 		return getString(val);
 	}
 
 	public Long getNextNumber(String basePath) {
-		return getNextNumber(basePath, false, 1);
+		return getNextNumber(basePath, 1);
 	}
 
-	public Long getNextNumber(String basePath, boolean persistInDB) {
-		return getNextNumber(basePath, persistInDB, 1);
-	}
-
-	private Long getNextNumber(String basePath, boolean persistInDB, Integer count) {
-		RAtomicLong atomicLong = redissonClient.getAtomicLong(NEW_LOGIC_KEY);
-		long newLogic = atomicLong.get();
-		if (newLogic > 0) {
-			return getNextNumberV2(basePath);
-		} else {
-			BoundHashOperations<String, String, Integer> x = redisTemplate.boundHashOps(basePath);
-			Long val = x.increment(basePath, count);
-			save(basePath, val);
-			return val;
-		}
+	private Long getNextNumber(String basePath, Integer count) {
+		Query query = new Query(Criteria.where("_id").is(basePath));
+		Update update = new Update().inc("value", count);
+		AutoIncrementEntity updatedDocument = mongoTemplate.findAndModify(query, update,
+				FindAndModifyOptions.options().returnNew(true).upsert(true), AutoIncrementEntity.class,
+				"auto_increment_entity");
+		return updatedDocument.getValue();
 	}
 
 	private String getString(Long val) {
@@ -102,46 +79,6 @@ public class UniqueUtilityService {
 		}
 		log.debug("unique string generated : {} for number {}", sb.toString(), copyVal);
 		return sb.reverse().toString();
-	}
-
-	public void save(String basePath, Long value) {
-		AutoIncrementEntity obj = new AutoIncrementEntity();
-		obj.setId(basePath);
-		obj.setValue(value);
-		mongoTemplate.save(obj);
-		log.info("auto increment: {}", obj);
-	}
-
-	public Long getNextNumberV2(String id) {
-		Query query = new Query();
-		Criteria criteria = Criteria.where("_id").is(id);
-		query.addCriteria(criteria);
-		Update update = new Update();
-		update.inc("value", 1L);
-		mongoTemplate.updateFirst(query, update, AutoIncrementEntity.class);
-		AutoIncrementEntity value = mongoTemplate.findById(id, AutoIncrementEntity.class);
-		if (ObjectUtils.isEmpty(value)) {
-			value = new AutoIncrementEntity();
-			value.setId(id);
-			value.setValue(1L);
-			platformCommonService.takeLock("CREATE_NEW_KEY" + id, 1, "key generation failed try again after 2 seconds",
-					LoggerType.ERROR);
-			mongoTemplate.save(value);
-		}
-		log.info("auto increment: {}", value);
-		return value.getValue();
-	}
-
-	public void enableNewLogic() {
-		RAtomicLong atomicLong = redissonClient.getAtomicLong(NEW_LOGIC_KEY);
-		atomicLong.set(10);
-		log.info("new logic in place");
-	}
-	
-	public void disableNewLogic() {
-		RAtomicLong atomicLong = redissonClient.getAtomicLong(NEW_LOGIC_KEY);
-		atomicLong.set(0);
-		log.info("new logic in place");
 	}
 
 }
