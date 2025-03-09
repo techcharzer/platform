0a020554caad76c4cecc8cd5ee97b031a4ba1a9f
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2025-03-09 15:39:44 +0530

    fixed.

---
+++ platform-core/src/main/java/com/cz/platform/DummyPlatformApplication.java
@@ -0,0 +1,21 @@
+//package com.cz.platform;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//
+//@SpringBootApplication
+//@EnableCaching
+//@EnableMongoRepositories(PlatformConstants.PARENT_PACKAGE)
+//@ComponentScan(basePackages = { PlatformConstants.PARENT_PACKAGE })
+//@EnableAsync
+//public class DummyPlatformApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DummyPlatformApplication.class, args);
+//	}
+//
+//}
