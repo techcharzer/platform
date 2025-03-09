942b1e672d136fdfe46f0821ef48e9eb4953d123
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2025-03-09 21:12:36 +0530

    fixed.

--- platform-core/src/main/java/com/cz/platform/logging/TraceFilter.java
+++ platform-core/src/main/java/com/cz/platform/logging/TraceFilter.java
@@ -9,35 +9,35 @@ import org.springframework.web.filter.GenericFilterBean;
 
 import com.cz.platform.PlatformConstants;
 
-import brave.Span;
-import brave.Tracer;
+import io.micrometer.tracing.TraceContext;
+import io.micrometer.tracing.Tracer;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @Order(value = Ordered.LOWEST_PRECEDENCE)
-class SleuthTraceFilter extends GenericFilterBean {
+class TraceFilter extends GenericFilterBean {
 
 	private final Tracer tracer;
 
-	SleuthTraceFilter(Tracer tracer) {
+	TraceFilter(Tracer tracer) {
 		this.tracer = tracer;
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		Span currentSpan = this.tracer.currentSpan();
-		if (currentSpan == null) {
-			chain.doFilter(request, response);
-			return;
+		log.info("trace filter configured");
+		TraceContext traceContext = this.tracer.currentTraceContext().context();
+		if (traceContext != null) {
+			// Add trace ID to response headers
+			((HttpServletResponse) response).addHeader(PlatformConstants.X_TRACE_ID, traceContext.traceId());
 		}
-		// for readability we're returning trace id in a hex form
-		((HttpServletResponse) response).addHeader(PlatformConstants.X_TRACE_ID, currentSpan.context().traceIdString());
 		chain.doFilter(request, response);
 	}
-
 }
