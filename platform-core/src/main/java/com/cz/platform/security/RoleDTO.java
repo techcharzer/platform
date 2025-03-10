d0bcf4dbe8e46285f43b23b047489dee1f906b28
Arun Chouhan <arun.chouhan@Aruns-MacBook-Pro.local>
2025-03-10 11:53:56 +0530

    fixed.

--- platform-core/src/main/java/com/cz/platform/security/RoleDTO.java
+++ platform-core/src/main/java/com/cz/platform/security/RoleDTO.java
@@ -4,8 +4,13 @@ import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 public class RoleDTO implements Serializable {
 	/**
 	 * 
